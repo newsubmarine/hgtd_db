5c5236a1365d2960512a360071bb90ca71cf89de
Shuiting Xin <shuiting.xin@cern.ch>
2021-08-20 13:41:54 +0800

    remove comments

--- SQLManager.py
+++ SQLManager.py
@@ -12,17 +12,17 @@ class SQLManager(object):
         self.conn = None
         self.cursor = None
         self.server = SSHTunnelForwarder(
-                (Server['host'], Server['port']),    #B机器的配置
+                (Server['host'], Server['port']),    
                 ssh_password=Server['pwd'],
                 ssh_username=Server['user'],
-                remote_bind_address=(DB['host'], DB['port'] ))  #A机器的配置
+                remote_bind_address=(DB['host'], DB['port'] )) 
         self.server.start()
         self.Connect()
 
     def Connect(self):
         DB = self.cfg['DB']
         print(self.server.local_bind_port)
-        self.conn = pymysql.Connect(host='127.0.0.1',              #此处必须是是127.0.0.1
+        self.conn = pymysql.Connect(host='127.0.0.1',              
                            port=self.server.local_bind_port,
                            user=DB['user'],
                            passwd=DB['pwd'],
