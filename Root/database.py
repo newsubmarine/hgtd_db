5b9aa3e14c467389e3f1b7d713ffce8b92832d9f
Shuiting Xin <shuiting.xin@cern.ch>
2022-01-25 21:57:50 +0100

    fix at lxplus

--- Root/database.py
+++ Root/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine, engine
 import logging
 import os
-import ConnHandler
+from Root.ConnHandler import ConnHandler
 logger = logging.getLogger("hgtd_db")
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
@@ -11,7 +11,8 @@ logging.getLogger().setLevel(logging.INFO)
 ## Only be calling creat_engine and session once !
 
 Base = declarative_base()
-connect = ConnHandler.ConnHandler(".config.yaml")
+connect = ConnHandler(".config.yaml")
+print(connect.get_engine_string())
 engine = create_engine(connect.get_engine_string())
 Session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 
